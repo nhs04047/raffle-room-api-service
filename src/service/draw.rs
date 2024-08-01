46efd506001c2be9f441d0fb64fa7a9d6bbed982
BoHyeon <nhs04047@gmail.com>
2024-08-01 23:58:10 +0900

    feat: alloc_draws 작성

--- src/service/draw.rs
+++ src/service/draw.rs
@@ -7,13 +7,37 @@ use crate::{
     draw_item as DrawItemRepository,
     joined_user as JoinedUserRepository
   },
-
 };
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 use super::structs;
 
-fn alloc_draws(darw_items: Vec<structs::DrawItem>, users: Vec<structs::User>) -> Vec<structs::Draw> {
-  
+fn alloc_draws(draw_items: Vec<structs::DrawItem>, users: Vec<structs::User>) -> Vec<structs::Draw> {
+
+  let mut draws = Vec::new();
+
+  for draw_item in &draw_items {
+    for i in 1..draw_item.qty {
+      let mut rng = thread_rng();
+      if let Some(random_element) = users.choose(&mut rng) {
+        println!("Selected: {}", random_element);
+
+        let draw = Draw {
+          id: draws.len() as i32 + 1,
+          room_id: 1, // example room_id
+          user_id: random_user.id,
+          draw_item_id: draw_item.id,
+          created_at: chrono::Utc::now().naive_utc(),
+        };
+       draws.push(draw);
+
+        let index = users.iter().position(|&x| x == *random_element).unwrap();
+        users.remove(index);
+    }
+    }
+  }
 }
 
 pub async fn get_draws_by_room_id(
